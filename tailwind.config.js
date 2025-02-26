e70ece0e7b34ff86e4a9462f5f50323510ad11e7
Sayfov Aliakbar <sayfovsliakbar.2009@gmail.com>
2025-02-26 17:32:04 +0500

    section 1 created

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     screens: {
-      'sm': '640px',
+      'sm': '10px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      'md': '1100px',
       // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
 
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
